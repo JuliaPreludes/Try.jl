21055cd1071c1f6bf0ca6d38d6babfb4c84d3bf3
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-21 09:14:26 -0400

    Setup Aqua

--- test/TryTests/src/TryTests.jl
+++ test/TryTests/src/TryTests.jl
@@ -5,6 +5,7 @@ include("test_errortrace.jl")
 include("test_tools.jl")
 include("test_inferrability.jl")
 include("test_show.jl")
+include("test_aqua.jl")
 include("test_doctest.jl")
 
 end  # module TryTests
