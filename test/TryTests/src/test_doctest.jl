ad397866d0e62765ede03ddab39cf4682c344b9d
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-21 06:45:22 -0700

    Run tests with Julia 1.6 (#34)

--- test/TryTests/src/test_doctest.jl
+++ test/TryTests/src/test_doctest.jl
@@ -5,10 +5,12 @@ using Try
 using TryExperimental
 
 function test_try()
+    VERSION < v"1.7" && return
     doctest(Try; manual = false)
 end
 
 function test_tryexperimental()
+    VERSION < v"1.7" && return
     doctest(TryExperimental; manual = false)
 end
 
