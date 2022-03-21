e6ee547128ba3a5bb9e83afeede17cec02d52db5
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-21 01:42:10 -0700

    Add show methods (#30)

--- test/TryTests/src/TryTests.jl
+++ test/TryTests/src/TryTests.jl
@@ -4,6 +4,7 @@ include("test_base.jl")
 include("test_errortrace.jl")
 include("test_tools.jl")
 include("test_inferrability.jl")
+include("test_show.jl")
 include("test_doctest.jl")
 
 end  # module TryTests
---
+++ test/TryTests/src/test_show.jl
@@ -0,0 +1,20 @@
+module TestShow
+
+using Test
+using Try
+
+Try.@function dummy
+
+function test_tryable()
+    @test string(dummy) == "dummy"
+    @test sprint(show, dummy) == "dummy"
+    @test sprint(show, "text/plain", dummy) == "dummy (tryable function with 1 method)"
+end
+
+function test_notimplementederror()
+    ex = Try.NotImplementedError(identity, (1, 2, 3), (a = 4, b = 5))
+    msg = sprint(showerror, ex)
+    @test occursin("identity(1, 2, 3; a = 4, b = 5)", msg)
+end
+
+end  # module
